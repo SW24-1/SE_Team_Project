b80994ade7a60e9b82aac51b3d1498e05d178108
lipdong <128962877+lipdong@users.noreply.github.com>
2024-05-29 21:08:08 +0800

    HomeController.java

--- SE_Team_Project-prototype/src/main/java/com/prototype/board/controller/HomeController.java
+++ SE_Team_Project-prototype/src/main/java/com/prototype/board/controller/HomeController.java
@@ -1,13 +1,38 @@
 package com.prototype.board.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/home")
-    public String index() {
-        System.out.println("HomeController.index");
-        return "index";
+
+    @GetMapping({"/dashboard","settings","/wiki","/bug_submit","/bug_search"})
+    public String dashboard(Model model){
+        return "dashboard";
     }
+    @GetMapping({"/settings","/dashboard","/bug_submit","/bug_search"})
+    public String setting(Model model) {
+        return "settings";
+    }
+    @GetMapping({"/dashboard"})
+    public String wiki(Model model) {
+        return "wiki";
+    }
+    @GetMapping({"/register","/dashboard","/list"})
+    public String index(Model model) {
+        return "index"; //
+    }
+    @GetMapping({"/list","/index"})
+    public String register(Model model){
+        return "register";
+    }
+
+    @GetMapping({"/register","/index"})
+    public String list(Model model){
+        return "list";
+    }
+
+
+
 }
